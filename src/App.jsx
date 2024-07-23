99acb852ce6a5e575a8e2c01a3d16601862497db
aswinss18 <aswinartography18@gmail.com>
2024-07-23 11:19:01 +0530

    Design of the website is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Home from "./pages/home/Home";
 import Profile from "./components/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./components/register/Register";
 function App() {
-  return <Profile />;
+  return <Home />;
 }
 
 export default App;
