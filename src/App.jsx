9e9764b660afc8edd57b395911121226405f3956
aswinss18 <aswinartography18@gmail.com>
2024-07-26 15:42:44 +0530

    Design completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,23 @@ import Home from "./pages/home/Home";
 import Profile from "./components/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./components/register/Register";
-function App() {
-  return <Home />;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App({ children }) {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
