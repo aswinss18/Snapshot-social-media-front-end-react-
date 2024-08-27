b7213320fb2e2f315a9dbf35f89a7f54de008c52
aswinss18 <aswinartography18@gmail.com>
2024-08-27 16:26:41 +0530

    Follow and unfollow a user functionality is created

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,28 +1,55 @@
 import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { Add, Remove } from "@mui/icons-material";
 export default function Rightbar({ user }) {
   const PF = import.meta.env.VITE_APP_PUBLIC_FOLDER || "";
   const baseurl = import.meta.env.VITE_APP_BASE_URL;
   const [friends, setFriends] = useState([]);
-  const HomeRightbar = () => {
-    useEffect(() => {
-      const getFriends = async () => {
-        try {
-          const friendList = await axios.get(
-            `${baseurl}/users/friends/${user._id}`
-          );
-          setFriends(friendList.data);
-        } catch (err) {
-          console.error(err);
-        }
-      };
-      getFriends();
-    }, [user._id]);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  const [followed, setFollowed] = useState(
+    currentUser.followings.includes(user?.id)
+  );
 
+  useEffect(() => {
+    setFollowed(currentUser.followings.includes(user?.id));
+  }, [currentUser, user.id]);
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const friendList = await axios.get(
+          `${baseurl}/users/friends/${user?._id}`
+        );
+        setFriends(friendList.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getFriends();
+  }, [user?._id]);
+  const handleClick = async () => {
+    try {
+      if (followed) {
+        await axios.put(`${baseurl}/users/${user._id}/unfollow`, {
+          userId: currentUser._id,
+        });
+        dispatch({ type: "UNFOLLOW", payload: user._id });
+      } else {
+        await axios.put(`${baseurl}/users/${user._id}/follow`, {
+          userId: currentUser._id,
+        });
+        dispatch({ type: "FOLLOW", payload: user._id });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    setFollowed(!followed);
+  };
+  const HomeRightbar = () => {
     return (
       <>
         <div className="birthdayContainer">
@@ -46,6 +73,12 @@ export default function Rightbar({ user }) {
     const PF = import.meta.env.VITE_APP_PUBLIC_FOLDER || "";
     return (
       <>
+        {user.username !== currentUser.username && (
+          <button className="rightbarFollowButton" onClick={handleClick}>
+            {followed ? "Unfollow" : "Follow"}
+            {followed ? <Remove /> : <Add />}
+          </button>
+        )}
         <h4 className="rightbarTitle">User information</h4>
         <div className="rightbarInfo">
           <div className="rightbarInfoItem">
